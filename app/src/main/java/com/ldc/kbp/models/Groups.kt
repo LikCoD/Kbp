a7f10d9974d87e03fd5fe67219dd2c21480cc2e1
LikDan <likdan.company@gmail.com>
2021-09-24 14:24:14 +0300

    Optimization

--- app/src/main/java/com/ldc/kbp/models/Groups.kt
+++ app/src/main/java/com/ldc/kbp/models/Groups.kt
@@ -6,27 +6,27 @@ import org.jsoup.Jsoup
 object Groups {
     @Serializable
     data class Timetable(val group: String, val link: String, val categoryIndex: Int)
-        fun loadTimetable(){
-            if (timetable.isEmpty()) {
-                Jsoup.connect("http://kbp.by/rasp/timetable/view_beta_kbp/?q=").get()
-                    .getElementsByClass("block_back")[0].children().drop(1).map {
-                    val category = it.select("span").text()
-                    val a = it.select("a")
 
-                    val index =
-                        if (categories.contains(category)) categories.indexOf(category) else {
-                            categories.add(category)
-                            categories.size - 1
-                        }
+    fun loadTimetable() {
+        if (timetable.isEmpty()) {
+            Jsoup.connect("http://kbp.by/rasp/timetable/view_beta_kbp/?q=").get()
+                .getElementsByClass("block_back")[0].children().drop(1).map {
+                val category = it.select("span").text()
+                val a = it.select("a")
 
-                    timetable.add(Timetable(a.text(), a.attr("href"), index))
+                val index = if (categories.contains(category)) categories.indexOf(category) else {
+                    categories.add(category)
+                    categories.size - 1
                 }
 
-                timetable.sortedBy { it.categoryIndex }
-                timetable = timetable.distinct().toMutableList()
+                timetable.add(Timetable(a.text(), a.attr("href"), index))
             }
-        }
 
-        var timetable: MutableList<Timetable> = mutableListOf()
-        var categories: MutableList<String> = mutableListOf()
+            timetable.sortedBy { it.categoryIndex }
+            timetable = timetable.distinct().toMutableList()
+        }
+    }
+
+    var timetable: MutableList<Timetable> = mutableListOf()
+    var categories: MutableList<String> = mutableListOf()
 }
