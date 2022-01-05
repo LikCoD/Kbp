3c3d4a6c222f46e606d43e791515c2a861670013
LikDan <likdan.company@gmail.com>
2022-01-05 13:19:57 +0300

    Change server url

--- app/src/main/java/com/ldc/kbp/models/Groups.kt
+++ app/src/main/java/com/ldc/kbp/models/Groups.kt
@@ -16,7 +16,7 @@ object Groups {
     data class SimpleInfo(val name: String, val id: String)
 
     fun loadTimetable() {
-        timetable = Json.decodeFromStream(URL("https://college-helper.herokuapp.com/schedule/types?educationPlaceId=0").openStream())
+        timetable = Json.decodeFromStream(URL("https://collegehelper-3f572.oa.r.appspot.com/schedule/types?educationPlaceId=0").openStream())
     }
 
     fun loadGroupsFromJournal() {
